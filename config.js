fda9c602c44cfe73d339e3d10a1999226f590707
borrageiros <66881294+borrageiros@users.noreply.github.com>
2024-01-18 19:33:24 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 module.exports = {
     app: {
-        token: "NzgxMjEyNzI5NjU0MjQ3NDM1.GAxMEY.1E-perFm0FoylLyzBzcWIFYEsUprtUkXSk0D28",
+        token: "",
         author: 'borrageiros',
-        guild: '759867160859377684',
+        guild: '',
         activityType: "Playing",
-        activity: "música",
-        invite: "https://discord.com/api/oauth2/authorize?client_id=773268275438026772&permissions=277028538368&scope=bot",
+        activity: "music",
+        invite: "",
         discordPlayer: {
             ytdlOptions: {
                 quality: 'highestaudio',
